0b242fb62bcfe73638804ad182b7b4b9f62dd6b7
Junaid Farooq <junaid@evercam.io>
2020-03-07 23:17:03 +0500

    Corrected API response and make API use.

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,15 +1,14 @@
 import { GET_ERRORS, SET_CURRENT_USER } from "./types"
-import axios from "axios"
-
+import API from "../utils/API"
 // Register User
 export const registerUser = (userData, history) => dispatch => {
-  axios
+  API
     .post("/api/users/register", userData)
     .then(res => history.push("/login"))
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err.response.data.errors
       })
     )
 }

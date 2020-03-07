270efe3fcf13faea19eb7bedb0ea6a0e5c2ea19a
Junaid Farooq <junaid@evercam.io>
2020-03-07 19:23:57 +0500

    Use Grid in App.js.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+
+import Register from "./components/auth/Register";
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+              <Route exact path="/register" component={Register} />
+            </Container>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;

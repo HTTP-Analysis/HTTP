82835bfc0132094e20fec9e29cb88c2c7688a53e
Junaid Farooq <junaid@evercam.io>
2020-03-14 18:31:45 +0500

    Changed API code to use API url and Token.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,23 @@
 import axios from "axios"
 
-export default axios.create({
-  baseURL: "http://localhost:4000",
-  responseType: "json"
-})
+const API = () => {
+  const defaultOptions = {
+    baseURL: "http://localhost:4000",
+    responseType: "json",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  let instance = axios.create(defaultOptions)
+
+  instance.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('jwtToken')
+    config.headers.Authorization =  token ? `Bearer ${token}` : ''
+    return config
+  })
+
+  return instance
+}
+
+export default API()
